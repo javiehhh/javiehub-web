cd2a3062400c0e838478e4bcb9ed21c03bb94127
javierehz <javiehz@hotmail.com>
2026-07-13 11:20:08 +0200

    About Me Component at Home Page

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { TechStack } from './components/tech-stack/tech-stack.component';
 import { Hero } from './components/hero/hero.component';
 import { FeaturedProjects } from './components/featured-projects/featured-projects.component';
+import { AboutMe } from './components/about-me/about-me.component';
 
 @Component({
   selector: 'app-home',
-  imports: [TechStack, Hero, FeaturedProjects],
+  imports: [TechStack, Hero, FeaturedProjects, AboutMe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
