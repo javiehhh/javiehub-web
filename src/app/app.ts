fe92027ef8add3c7f2f2c7fb5e88b04e2fb1f551
javierehz <javiehz@hotmail.com>
2026-07-13 12:11:27 +0200

    UI Improvements

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/components/navbar/navbar.component';
+import { Footer } from './shared/components/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/features/home/components/hero/hero.component.ts
+++ src/app/features/home/components/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -7,6 +7,34 @@ import { RouterLink } from '@angular/router';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
-export class Hero {
-  
+export class Hero implements OnInit{
+   titles = [
+    'Java Developer',
+    'Spring Boot Developer',
+    'Angular Developer',
+    'Kotlin Developer'
+  ];
+
+  currentTitle = signal(this.titles[0]);
+
+  currentIndex = 0;
+
+  ngOnInit(): void {
+
+    setInterval(() => {
+
+      this.currentIndex++;
+
+      if (this.currentIndex >= this.titles.length) {
+        this.currentIndex = 0;
+      }
+
+      this.currentTitle.set(
+        this.titles[this.currentIndex]
+      );
+
+    }, 2500);
+
+  }
+
 }
